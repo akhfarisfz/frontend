8d366c23e1186957ea307d1de0db5c90fbfd811e
akhfarisfz <akhfarifz6017@gmail.com>
2024-08-27 07:17:52 +0700

    Changes

--- src/page/Essay.js
+++ src/page/Essay.js
@@ -4,7 +4,7 @@ import stringSimilarity from 'string-similarity';
 import axios from 'axios';
 import io from 'socket.io-client';
 
-const socket = io('webkonsep-backend-q6ab3ichy-akhfarisfzs-projects.vercel.app'); // Connect to the server
+const socket = io('https://webkonsep-backend.vercel.app/'); // Connect to the server
 // const socket = io('localhost'); // Connect to the server
 
 const Essay = () => {
